3ee0a2e62f670d7de9fcaf1ee80128f80c903039
Klein Reveche <therealklr2003@gmail.com>
2024-07-27 07:09:13 +0800

    Add animation to main screen

--- composeApp/src/commonMain/kotlin/presentation/navigation/Navigation.kt
+++ composeApp/src/commonMain/kotlin/presentation/navigation/Navigation.kt
@@ -1,5 +1,9 @@
 package presentation.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,17 +22,62 @@ fun Navigation() {
     vm.saveDeviceIdentifier()
 
     NavHost(navController = navController, startDestination = ScreenMain) {
-        composable<ScreenMain> { ScreenMain(navController) }
-        composable<ScreenLocalVsComputer> {
+        composable<ScreenMain>(
+            enterTransition = {
+                return@composable fadeIn(tween(1000))
+            }, exitTransition = {
+                return@composable slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start, tween(700)
+                )
+            }, popEnterTransition = {
+                return@composable slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End, tween(700)
+                )
+            }
+        ) { ScreenMain(navController) }
+        composable<ScreenLocalVsComputer>(
+            enterTransition = {
+                return@composable slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start, tween(700)
+                )
+            },
+            popExitTransition = {
+                return@composable slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End, tween(700)
+                )
+            },
+        ) {
             val data = it.toRoute<ScreenLocalVsComputer>()
             vm.addVsComputerPlayer(data)
             ScreenLocalVsComputer(data, navController)
         }
-        composable<ScreenLocalVsPlayer> {
+        composable<ScreenLocalVsPlayer>(
+            enterTransition = {
+                return@composable slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start, tween(700)
+                )
+            },
+            popExitTransition = {
+                return@composable slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End, tween(700)
+                )
+            },
+        ) {
             val data = it.toRoute<ScreenLocalVsPlayer>()
             vm.addVsPlayerPlayers(data)
             ScreenLocalVsPlayer(data, navController)
         }
-        composable<ScreenMultiplayer> {}
+        composable<ScreenMultiplayer>(
+            enterTransition = {
+                return@composable slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start, tween(700)
+                )
+            },
+            popExitTransition = {
+                return@composable slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End, tween(700)
+                )
+            },
+        ) {}
     }
 }
