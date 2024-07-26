c7b8f0113101d1d7d500513afe3cad6b507badc8
Klein Reveche <therealklr2003@gmail.com>
2024-07-26 19:28:17 +0800

    Add loading splash to wasmJs target

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -3,9 +3,12 @@ import androidx.compose.ui.window.ComposeViewport
 import di.initKoin
 import kotlinx.browser.document
 
+external fun onLoadFinished()
+
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
+    onLoadFinished()
     ComposeViewport(document.body!!) {
         TicTacToeApp()
     }
